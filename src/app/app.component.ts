f8b8459be3b061dc0dcbd7c916fbe77b51984ecc
Prithiviraj Ramalingam <Prithiviraj.Ramalingam@pfcb.com>
2018-02-15 14:59:24 +0530

    Farcry 3 Loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    words:string[] = ["PRITHIVI", "UI DEVELOPER", "CAMERA", "PHOTOGRAPHY", "WEBSITE", "CSS", "HTML", "JAVASCRIPT", "JQUERY", "ANGULAR", "FRONTEND", "GIGYA", "BOOTSTRAP"];
+
+    //function for generating random value.
+     getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //function for changing the word
+    changeWord(a) {
+		a.style.opacity = '0.1';
+		a.innerHTML = this.words[this.getRandomInt(0, this.words.length - 1)];
+		setTimeout(function() {
+				a.style.opacity = '1';
+        }, 425);
+        setTimeout(()=>{    //<<<---    using ()=> syntax
+            this.changeWord(a);
+       },this.getRandomInt(500, 800));
+    }
+
+    ngAfterViewInit() {
+       var randoms= window.document.getElementsByClassName("randoms");
+        for (var i = 0; i < randoms.length; i++)
+                this.changeWord(randoms[i]);
+
+    }
 }
